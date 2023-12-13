cb212cebfbca408ab504f0325803919f26cb58f0
tasosApostolou <thoukis95@gmail.com>
2023-12-13 12:34:26 +0200

    main updated with a new print

--- src/gr/aueb/cf/gitNew/Main.java
+++ src/gr/aueb/cf/gitNew/Main.java
@@ -3,5 +3,6 @@ package gr.aueb.cf.gitNew;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("new print");
     }
 }
